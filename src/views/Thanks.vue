4cc811d4d0d92866ab1b75ecd232279e73c9c32c
Price Comstock <price.comstock@gmail.com>
2018-10-08 14:36:01 -0500

    added Thanks page

---
+++ src/views/Thanks.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="thanks">
+    <p>Thanks for adding Inspo-paper to Slack!</p>
+    <p>Give it a try by clicking the <button class="button is-small is-static"><span class="icon is-small"><i class="fas fa-ellipsis-h"></i></span></button> next to a slack message!</p>
+  </div>
+</template>
